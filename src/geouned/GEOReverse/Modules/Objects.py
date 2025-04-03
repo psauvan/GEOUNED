9c81b44a7bd2df3c7a2b3282386a2556760a56ca
Patrick Sauvan <psauvan@ind.uned.es>
2025-04-03 11:18:15 +0200

    Revert "Merge branch 'dev_2.0_alpha' into boolean_region"
    This reverts commit 15869b19dd53ccf979bec7739e00748f3baff83c, reversing
    changes made to 7fd519e6871c3611bb4fc686b86435c23462c953.

--- src/geouned/GEOReverse/Modules/Objects.py
+++ src/geouned/GEOReverse/Modules/Objects.py
@@ -31,8 +31,6 @@ class CadCell:
             self.MAT = 0  # material number
             self.CurrentTR = None
             self.level = None
-            self.cell_seq = None
-            self.hash_def = None
             self.__defTerms__ = None
             self.__operator__ = None
             self.externalBox = None
@@ -49,8 +47,6 @@ class CadCell:
             self.MAT = stringCell.MAT  # material number
             self.CurrentTR = self.TRFL
             self.level = None
-            self.cell_seq = stringCell.cellSeq
-            self.hash_def = stringCell.hashDef
 
             self.__defTerms__ = None
             self.__operator__ = None
@@ -82,17 +78,12 @@ class CadCell:
         cpCell.FILL = self.FILL
         cpCell.MAT = self.MAT
         cpCell.level = self.level
-        cpCell.hash_def = self.hash_def
-        cpCell.cell_seq = self.cell_seq
 
         if self.CurrentTR is not None:
             cpCell.CurrentTR = self.CurrentTR.submatrix(4)
 
         if self.shape is not None:
             cpCell.shape = self.shape.copy()
-
-        if self.BoundBox is not None:
-            cpCell.BoundBox = FreeCAD.BoundBox(self.BoundBox)
 
         return cpCell
 
