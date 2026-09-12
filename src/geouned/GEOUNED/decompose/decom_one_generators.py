008fd6b13515254f815b9c4d28815ae6d2bc7a1c
Patrick Sauvan <psauvan@ind.uned.es>
2026-09-12 09:14:48 +0200

    clean

--- src/geouned/GEOUNED/decompose/decom_one_generators.py
+++ src/geouned/GEOUNED/decompose/decom_one_generators.py
@@ -26,11 +26,6 @@ def generic_split(solid, options, tolerances, loop=0):
     bbox = solid.BoundBox
     bbox.enlarge(10)
 
-    stpWR = True
-    if stpWR:
-        solid.exportStep("tempSol.stp")
-        solid = Part.read("tempSol.stp")
-
     cleaned = [solid]
     omitfaces = set()
 
